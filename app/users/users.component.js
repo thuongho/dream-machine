e499140d80af143e0607c48310e47fc7b3092792
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-21 21:18:21 -0700

    remove starter title

--- app/users/users.component.js
+++ app/users/users.component.js
@@ -19,7 +19,7 @@ var UsersComponent = (function () {
         var _this = this;
         this._userService.getUsers()
             .subscribe(function (users) {
-            console.log(users);
+            // console.log(users);
             _this.users = users;
         });
     };
--- app/users/users.component.ts
+++ app/users/users.component.ts
@@ -17,7 +17,7 @@ export class UsersComponent implements OnInit {
   ngOnInit() {
     this._userService.getUsers()
       .subscribe(users => {
-        console.log(users);
+        // console.log(users);
         this.users = users;
       });
   }
