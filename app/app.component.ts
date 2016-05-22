73aa2ddcee4e811a9871bd2c54cf9e223b213cb2
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-21 17:38:38 -0700

    add header component with bootstrap cerulean theme navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import {Component} from '@angular/core';
 
+import {HeaderComponent} from './header/header.component';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+      <dream-header></dream-header>
+      <h1>My First Angular 2 App</h1>
+    `,
+    directives: [HeaderComponent]
 })
+
 export class AppComponent { }
